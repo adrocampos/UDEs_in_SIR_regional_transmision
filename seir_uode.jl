5bd7b520be0a645b1b2aab37e2e312c1f5bbe56e
adrocampos <rrojascampos@uni-osnabrueck.de>
2022-12-05 11:28:38 +0100

    update

--- seir_uode.jl
+++ seir_uode.jl
@@ -1,32 +1,26 @@
 # ## Environment and packages
-# cd(@__DIR__)
+cd(@__DIR__)
 using Pkg;
-# Pkg.activate(".");
+Pkg.activate(".");
 
 using OrdinaryDiffEq
 using ModelingToolkit
 using DataDrivenDiffEq
 using LinearAlgebra, Optim
 using DiffEqFlux, Flux
-
 using Random
 using CSV
 using Lux
 using DataFrames
-
-
-
-Pkg.status()
-
-
+using JLD
+using Plots
+gr()
 
 data_dir = "/Users/adrocampos/covid19/synth_data/"
 regions = ["2", "3", "5", "10", "15", "20", "30"][1]
 mobility_type = ["inv_dist", "border", "neighbor"][2]
 initially_recovered = false
 
-# positions = CSV.File(data_dir * "positions_" * regions * "_regions.csv")
-# positions = DataFrame(positions)
 
 file = "1"
 
@@ -34,186 +28,98 @@ csv_reader = CSV.File(data_dir * "SIR_" * regions * "_regions_" * mobility_type 
 df = DataFrame(csv_reader)
 
 
-index = range(1, stop=5001, step=50)
+index = range(1, stop=5001, step=200)
 df = df[index, :]
 
-X = Matrix(df[:, [:S1, :I1, :R1]])'
-t = df.t
+X = Matrix(df[:, [:S1, :I1, :R1]])
+X = transpose(X)
+println("size(X) =", size(X))
 
-tspan = (t[begin], t[end])
-tsteps = range(tspan[1], tspan[2], length=size(t)[1])
+
+
+tspan = (1, size(X)[2])
+
+t = range(tspan[1], tspan[2], step=1)
 
 u0 = X[:, 1]
 
+# ann = FastChain(
+#     FastDense(3, 32, tanh), FastDense(32, 32, tanh), FastDense(32, 1)
+# )
 
-ann = FastChain(
-    FastDense(3, 64, tanh), FastDense(64, 64, tanh), FastDense(64, 1)
-)
-
-p = initial_params(ann)
+# p = Float64.(initial_params(ann))
 
 
-# rng = Random.default_rng()
-# ann = Lux.Chain(Lux.Dense(3, 50, tanh),
-#     Lux.Dense(50, 50, tanh),
-#     Lux.Dense(50, 1))
+rng = Random.default_rng()
+ann = Lux.Chain(Lux.Dense(3, 32, NNlib.tanh), 
+                Lux.Dense(32, 32, NNlib.tanh), 
+                Lux.Dense(32, 1))
 
-# p, st = Lux.setup(rng, ann)
+# Initialize the model weights and state
+p, st = Lux.setup(rng, ann)
 
-# p = [rand(Float32, 2); p]
-
-## p here and p outside? Overwrite?
-
-p_ = [0.1, 0.1, 2000]
 
 function dudt!(du, u, p, t)
-    
+
+    N = sum(u)
     S, I, R = u
-    β, γ, N  = p_
+    β = 0.1
+    γ = 1 / 10
 
-    z  = ann(u, p)
-    dS = -β * S * I/N - z[1]  # susceptible
-    dI =  β * S * I/N #- γ*I - z[1] # infected
-    dR =  γ * I
+    z = ann(u, p)
 
-    print("z =", z, "u =", u)
-
-    du[1] = dS
-    du[2] = dI
-    du[3] = dR
+    du[1] = dS = -β * S * I / N - z[1]
+    du[2] = dI = β * S * I / N + z[1] - γ * I
+    du[3] = dR = γ * I
 
 end
 
 
 
 
-# # Define the problem
+## Define the problem
+## Prediction with initial random weights
 prob_UODE = ODEProblem(dudt!, u0, tspan, p) ##prob_neuralode
-# # prob_UODE = NeuralODE(dudt!, tspan, Tsit5(),saveat=tsteps) ##prob_neuralode
-
-
-
+s = concrete_solve(prob_UODE, Tsit5(), u0, p, saveat=t)
 
 ## Function to train the network
 # Define a predictor
-function predict(θ, X=X[:, 1], T=t)
-    Array(solve(prob_UODE, Vern7(), u0=X, p=θ,
-        tspan=(T[1], T[end]), saveat=T,
-        abstol=1e-6, reltol=1e-6,
+function predict(θ)
+    Array(concrete_solve(prob_UODE, Vern7(), u0=u0, p=θ,
+        tspan=tspan, saveat=1, abstol=1e-6, reltol=1e-6,
         sensealg=ForwardDiffSensitivity()
     ))
 end
+a = predict(p)
 
+pS = plot(t, [X[1, :], a[1, :]], label=["data S" "prediction S"])
+pI = plot(t, [X[2, :], a[2, :]], label=["data I" "prediction I"])
+pR = plot(t, [X[3, :], a[3, :]], label=["data R" "prediction R"])
+
+display(plot(pS, pI, pR, layout=(3, 1), size=(800, 800)))
 
 ## .- instead of -?
 function loss(θ)
     X̂ = predict(θ)
-    sum(abs2, X .- X̂) #/ size(X, 2) + convert(eltype(θ), 1e-3) * sum(abs2, θ[3:end]) ./ length(θ[3:end])
+    sum(abs2, X .- X̂) / size(X, 2) + convert(eltype(θ), 1e-3) * sum(abs2, θ[3:end]) ./ length(θ[3:end])
 end
+println("initial loss = ", loss(p))
 
-# Container to track the losses
 losses = Float32[]
-
-# Callback to show the loss during training
 callback(θ, args...) = begin
     l = loss(θ) # Equivalent L2 loss
-    println("l = ", l)
     push!(losses, l)
-    # if length(losses) % 5 == 0
-    #     println("Current loss after $(length(losses)) iterations: $(losses[end])")
-    # end
+    if length(losses) % 5 == 0
+        println("Current loss after $(length(losses)) iterations: $(losses[end])")
+    end
     false
 end
 
-# # use Optimization.jl to solve the problem
 
-# # pinit = Lux.ComponentArray(p)
-# # adtype = Optimization.AutoZygote()
-# # optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
-# # optprob = Optimization.OptimizationProblem(optf, pinit)
+pinit = Lux.ComponentArray(p)
+adtype = Optimization.AutoForwardDiff()
+optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
+optprob = Optimization.OptimizationProblem(optf, pinit)
 
-# # result_neuralode = Optimization.solve(optprob,
-# #     ADAM(0.01),
-# #     # callback=callback,
-# #     maxiters=1)
-
-res1_uode = DiffEqFlux.sciml_train(loss, p, ADAM(0.001),  maxiters=1)
-
-
-
-## Training -> First shooting / batching to get a rough estimate
-
-# First train with ADAM for better convergence -> move the parameters into a
-# favourable starting positing for BFGS
-# res1 = DiffEqFlux.sciml_train(shooting_loss, p, ADAM(0.1f0), cb=callback, maxiters=100)
-# println("Training loss after $(length(losses)) iterations: $(losses[end])")
-# # Train with BFGS to achieve partial fit of the data
-# res2 = DiffEqFlux.sciml_train(shooting_loss, res1.minimizer, BFGS(initial_stepnorm=0.01f0), cb=callback, maxiters=500)
-# println("Training loss after $(length(losses)) iterations: $(losses[end])")
-# # Full L2-Loss for full prediction
-# res3 = DiffEqFlux.sciml_train(loss, res2.minimizer, BFGS(initial_stepnorm=0.01f0), cb=callback, maxiters=100)
-# println("Final training loss after $(length(losses)) iterations: $(losses[end])")
-
-
-
-# function predict(θ, X=X[:, 1], T=t)#     Array(solve(prob_UODE, Vern7(), u0=X, p=θ,
-#         tspan=(T[1], T[end]), saveat=T,
-#         abstol=1e-6, reltol=1e-6,
-#         sensealg=ForwardDiffSensitivity()
-#     ))
-# end
-
-# function loss(θ)
-#     X̂ = predict(θ)
-#     sum(abs2, X - X̂) / size(X, 2) + convert(eltype(θ), 1e-3) * sum(abs2, θ[3:end]) ./ length(θ[3:end])
-# end
-
-
-
-
-
-
-# # Container to track the losses
-# losses = Float32[]
-
-# # Callback to show the loss during training
-# callback(θ, args...) = begin
-#     l = loss(θ) # Equivalent L2 loss
-#     push!(losses, l)
-#     if length(losses) % 5 == 0
-#         println("Current loss after $(length(losses)) iterations: $(losses[end])")
-#     end
-#     false
-# end
-
-
-
-
-
-
-
-
-
-# res_fit = DiffEqFlux.sciml_train(loss, p, BFGS(initial_stepnorm=0.1f0), cb=callback, maxiters=2)
-# ## Training -> First shooting / batching to get a rough estimate
-
-# # First train with ADAM for better convergence -> move the parameters into a
-# # favourable starting positing for BFGS
-
-
-# # res1 = DiffEqFlux.sciml_train(shooting_loss, p, Adam(0.1f0), cb=callback, maxiters=1)
-# # println("Training loss after $(length(losses)) iterations: $(losses[end])")
-# # Train with BFGS to achieve partial fit of the data
-# # res2 = DiffEqFlux.sciml_train(shooting_loss, res1.minimizer, BFGS(initial_stepnorm=0.01f0), cb=callback, maxiters = 500)
-# # println("Training loss after $(length(losses)) iterations: $(losses[end])")
-# # # Full L2-Loss for full prediction
-# # res3 = DiffEqFlux.sciml_train(loss, res2.minimizer, BFGS(initial_stepnorm=0.01f0), cb=callback, maxiters = 10000)
-# # println("Final training loss after $(length(losses)) iterations: $(losses[end])")
-
-# # use Optimization.jl to solve the problem
-# # adtype = Optimization.AutoZygote()
-
-# optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
-# optprob = Optimization.OptimizationProblem(optf, p)
-
-# result_neuralode = Optimization.solve(optprob, ADAM(0.005), tspan=tspan, saveat=t, maxiters=10)
+## Gives the parameters back!
+res1_uode = Optimization.solve(optprob, Adam(0.01, (0.8, 0.8)), callback=callback, maxiters=10)
